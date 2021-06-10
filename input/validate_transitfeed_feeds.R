d950da9d2abd05f41c0e7bf8837241a0c285d8a0
Eric Chandler <echandler@cs.uchicago.edu>
2021-06-10 14:14:39 -0500

    add comments

--- input/validate_transitfeed_feeds.R
+++ input/validate_transitfeed_feeds.R
@@ -11,24 +11,31 @@ blacklist <- tibble(feed=character(), reason=character())
 feeds_path <- file.path("input/shared/feeds")
 feed_names <- list.files(feeds_path, pattern='.*.zip')
 for (feed_name in feed_names) {
+  # Try reading feed into data frame
   feed_path <- file.path(feeds_path, feed_name)
   feed <- tryCatch(read_gtfs(feed_path), error=function(e){print(e);NULL})
   if (is.null(feed)) {
     blacklist <- blacklist %>% add_row(feed=feed_name, reason="cant read")
     next;
   }
+
+  # Try converting feed data frame into sf
   feed_obj <- tryCatch(gtfs_as_sf(feed), error=function(e){
     print(paste("ERROR:", feed_path)); print(e);NULL})
   if (is.null(feed_obj)) {
     blacklist <- blacklist %>% add_row(feed=feed_name, reason="cant convert sf")
     next;
   }
+
+  # Try extracting route geometries
   geoms <- tryCatch(suppressWarnings(get_route_geometry(feed_obj)), error=function(e){
     print(paste("ERROR:", feed_path)); print(e);NULL})
   if (is.null(geoms)) {
     blacklist <- blacklist %>% add_row(feed=feed_name, reason="cant construct geom")
     next;
   }
+
+  # Check validity of route geometries
   for (route_idx in 1:nrow(geoms)) {
     geom <- geoms$geometry[route_idx]
     route_id <- geoms$route_id[route_idx]
--- input/validate_transitland_feeds.R
+++ input/validate_transitland_feeds.R
@@ -13,25 +13,33 @@ for (state_abbr in state_dirs) {
   state_path <- file.path("input/shared/feeds/transitland", state_abbr)
   feed_names <- list.files(state_path, pattern='.*.zip')
   print(paste("Checking state", state_abbr))
+
   for (feed_name in feed_names) {
+    # Try reading feed into data frame
     feed_path <- file.path(state_path, feed_name)
     feed <- tryCatch(read_gtfs(feed_path), error=function(e){print(e);NULL})
     if (is.null(feed)) {
       blacklist <- blacklist %>% add_row(state=state_abbr, feed=feed_name, reason="cant read")
       next;
     }
+
+    # Try converting feed data frame into sf
     feed_obj <- tryCatch(gtfs_as_sf(feed), error=function(e){
       print(paste("ERROR:", feed_path)); print(e);NULL})
     if (is.null(feed_obj)) {
       blacklist <- blacklist %>% add_row(state=state_abbr, feed=feed_name, reason="cant convert sf")
       next;
     }
+
+    # Try extracting route geometries
     geoms <- tryCatch(suppressWarnings(get_route_geometry(feed_obj)), error=function(e){
       print(paste("ERROR:", feed_path)); print(e);NULL})
     if (is.null(geoms)) {
       blacklist <- blacklist %>% add_row(state=state_abbr, feed=feed_name, reason="cant construct geom")
       next;
     }
+
+    # Check validity of route geometries
     for (route_idx in 1:nrow(geoms)) {
       geom <- geoms$geometry[route_idx]
       route_id <- geoms$route_id[route_idx]
@@ -42,6 +50,7 @@ for (state_abbr in state_dirs) {
     }
   }
 }
+
 print("List of invalid geos:")
 print(blacklist)
 write.csv(blacklist,"input/shared/feeds/transitland/blacklist.csv", row.names = FALSE)
